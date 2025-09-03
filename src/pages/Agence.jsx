554470dff57371ce3abd805427ce314bf181d349
ShamirAli55 <shamirali9779@gmail.com>
2025-09-03 17:17:26 +0500

    imgs added

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -4,6 +4,23 @@ import { ScrollTrigger } from "gsap/all";
 import gsap from "gsap";
 
 const Agence = () => {
+  const images = [
+    "/images/img1.jpg",
+    "/images/img2.jpg",
+    "/images/img3.jpg",
+    "/images/img4.jpg",
+    "/images/img5.jpg",
+    "/images/img6.jpg",
+    "/images/img7.jpg",
+    "/images/img8.jpg",
+    "/images/img9.jpg",
+    "/images/img10.jpg",
+    "/images/img11.jpg",
+    "/images/img12.jpg",
+    "/images/img13.jpg",
+    "/images/img14.jpg",
+  ];
+
   gsap.registerPlugin(ScrollTrigger);
   const imageDivRef = useRef(null);
   const imageRef = useRef(null);
@@ -12,11 +29,18 @@ const Agence = () => {
     gsap.from(imageDivRef.current, {
       scrollTrigger: {
         trigger: imageDivRef.current,
-        start: "top 27%",
-        end: "top -200%",
+        start: "top 28%",
+        end: "top -140%",
         scrub: true,
         pin: true,
         markers: true,
+        onUpdate: (self) => {
+          let imageIndex = Math.min(
+            Math.floor(self.progress * images.length),
+            images.length - 1
+          );
+          imageRef.current.src = images[imageIndex];
+        },
       },
     });
   });
@@ -40,7 +64,7 @@ const Agence = () => {
           </h1>
         </div>
         <div className="flex justify-end pr-8">
-          <p className="text-5xl w-1/2 font-semibold leading-[5vw]">
+          <p className="text-5xl w-[60%] font-semibold leading-[4vw]">
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre
             curiosité nourrit notre créativité. On reste humbles et on dit non
             aux gros egos, même le vôtre. Une marque est vivante. Elle a des
