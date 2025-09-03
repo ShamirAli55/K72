18778324052afc99a3e9368cbbe3541e650d3a62
ShamirAli55 <shamirali9779@gmail.com>
2025-09-03 19:13:23 +0500

    hidden navbar implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import Navbar from "./components/navigation/Navbar";
 import Agence from "./pages/Agence";
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <Router>
         <Route index path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
---
+++ src/components/navigation/FullScreenNavbar.jsx
@@ -0,0 +1,26 @@
+import {X} from "lucide-react";
+import { Link } from "react-router-dom";
+const FullScreenNavbar = ({setOpenMenu}) => {
+ const tags = [
+    {name:"Projects",to:"/projecrs"},
+    {name:"Agence",to:"/agence"},
+    {name:"Contact",to:"/contact"},
+    {name:"Blogue",to:"/blog"},
+  ]
+  return (
+    <div className="h-screen w-full z-50 fixed bg-black">
+        <X className="text-white absolute top-[-5%] right-[0] cursor-pointer" strokeWidth={.5} 
+        size={200} onClick={() => setOpenMenu(false)}/>
+
+        <div className="flex h-full w-full items-center flex-col justify-center pt-4">
+        {tags.map((tag,index)=>(
+            <Link 
+            className={`text-white text-8xl font-[font1] font-bold cursor-pointer flex uppercase
+            justify-center w-full  leading-[6vw] pt-4  border-b border-white/50 ${index == 0 && "border-t"}`} to={tag.to} key={index}>{tag.name}</Link>
+        ))}
+        </div>
+    </div>
+  )
+}
+
+export default FullScreenNavbar
