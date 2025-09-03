e15701c908879086cdefe3d736f127d1d398ebc0
ShamirAli55 <shamirali9779@gmail.com>
2025-09-03 16:55:15 +0500

    gsap implemented

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,21 +1,60 @@
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
 
 const Agence = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.from(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        start: "top 27%",
+        end: "top -200%",
+        scrub: true,
+        pin: true,
+        markers: true,
+      },
+    });
+  });
   return (
     <div>
-      <div className="h-70 w-50 absolute top-1/2 left-[30%] transform  -translate-y-1/2 rounded-2xl overflow-hidden">
-        <img className="h-full w-full object-cover"
-        src="/images/img1.jpg" alt="" />
+      <div
+        ref={imageDivRef}
+        className="h-64 w-50 absolute top-1/2 left-[30%] transform  -translate-y-1/2 rounded-2xl overflow-hidden"
+      >
+        <img
+          ref={imageRef}
+          className="h-full w-full object-cover"
+          src="/images/img1.jpg"
+          alt=""
+        />
       </div>
-    <div className="font-[font2] relative">
-    <div className="mt-[55vh]">
-      <h1 className="text-center text-[19vw] uppercase leading-[18vw] font-bold">Soixan7e Douze</h1>
-    </div>
-   <div className="flex justify-end pr-8">
-     <p className="text-5xl w-1/2 font-semibold leading-[5vw]">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
-   </div>
-    </div>
-    </div>
-  )
-}
+      <div className="font-[font2] relative">
+        <div className="mt-[55vh]">
+          <h1 className="text-center text-[19vw] uppercase leading-[18vw] font-bold">
+            Soixan7e Douze
+          </h1>
+        </div>
+        <div className="flex justify-end pr-8">
+          <p className="text-5xl w-1/2 font-semibold leading-[5vw]">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre
+            curiosité nourrit notre créativité. On reste humbles et on dit non
+            aux gros egos, même le vôtre. Une marque est vivante. Elle a des
+            valeurs, une personnalité, une histoire. Si on oublie ça, on peut
+            faire de bons chiffres à court terme, mais on la tue à long terme.
+            C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir
+            des marques influentes.
+          </p>
+        </div>
+      </div>
 
-export default Agence
+      <section className="h-screen bg-yellow-100"></section>
+    </div>
+  );
+};
+
+export default Agence;
