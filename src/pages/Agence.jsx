dcabc2662a704efbe7c1310a8c73f2e8b109201b
ShamirAli55 <shamirali9779@gmail.com>
2025-09-03 16:38:06 +0500

    gsap dependency installed

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
 
 const Agence = () => {
   return (
-    <div>Agence</div>
+    <div>
+      <div className="h-70 w-50 absolute top-1/2 left-[30%] transform  -translate-y-1/2 rounded-2xl overflow-hidden">
+        <img className="h-full w-full object-cover"
+        src="/images/img1.jpg" alt="" />
+      </div>
+    <div className="font-[font2] relative">
+    <div className="mt-[55vh]">
+      <h1 className="text-center text-[19vw] uppercase leading-[18vw] font-bold">Soixan7e Douze</h1>
+    </div>
+   <div className="flex justify-end pr-8">
+     <p className="text-5xl w-1/2 font-semibold leading-[5vw]">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
+   </div>
+    </div>
+    </div>
   )
 }
 
